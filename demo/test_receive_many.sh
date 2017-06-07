d5f4a606c63e8ea0d973fee29512919d9addbe40
Mateusz Morusiewicz <mmorusiewicz@newterm.pl>
2017-06-07 23:52:22 +0200

    Fixup of receive and send many test

--- demo/test_receive_many.sh
+++ demo/test_receive_many.sh
@@ -1,17 +1,16 @@
-./../client get -n 2 -s eq Ala -s any
+./../client get -n 2 -s eq ala -s any
 ./../client get -n 1 -s any
 ./../client get -n 2 -s eq ale -s any
 ./../client get -n 2 -s any -s any
-./../client get -n 3 -s any -s any -s .
+./../client get -n 2 -s any -s any 
 
 sleep 2
 
-./../client read -n eq 2 -i eq 13 -i eq 33
-./../client get -n any -i any
-./../client get -n lt 10 -i eq 32
-./../client get -n eq 1 -i eq 10
-./../client get -n eq 3 -i any -i eq 20 -i eq 30
-./../client get -n lt 5 -i gt 20 -i eq 28 -i any
-./../client get -n any -i eq 31 -i gt 6
-./../client get -n eq 2 -i eq13 -i eq 33
-./../client get -n any -i eq 20 -i gt 20 -s a
+./../client read -n eq 3 -i lt 30 -i gt 20 -i any
+./../client get -n 3 -i any -i le 42 -i ge 42
+./../client get -n 1 -i any
+./../client get -n 2 -i eq 31 -i any
+./../client get -n 1 -i gt 100
+./../client get -n 1 -i lt 100
+./../client get -n 3 -i any -i eq 20 -i gt 20
+./../client get -n 3 -i any -i eq 22 -s eq ?
